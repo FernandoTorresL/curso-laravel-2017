6d2bd20f45c9ab83b3e2ff8162976027ea56555a
Fernando Torres <fertorresmx@gmail.com>
2018-08-04 22:21:38 -0500

    Archivos video 05 Repeticiones y condicionales en templates de Blade

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $links = [
+        'https://platzi.com/laravel' => 'Curso de Laravel',
+        'https://laravel.com' => 'Página de Laravel'
+    ];
+
+    return view('welcome', [
+        'teacher' => 'Guido Contreras Woda',
+        'links' => $links,
+    ]);
+});
+
+Route::get('/acerca', function () {
+    return view('about');
+});
