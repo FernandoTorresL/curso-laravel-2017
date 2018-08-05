8d6f29f9f87a34952008907d97408c7b08dbd463
Fernando Torres <fertorresmx@gmail.com>
2018-08-05 00:10:26 -0500

    Archivos video 08 Agregar Bootstrap a nuestro proyecto

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -1,26 +1,34 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
 class PagesController extends Controller
 {
     public function home()
     {
-        $links = [
-            'https://platzi.com/laravel' => 'Curso de Laravel',
-            'https://laravel.com' => 'Página de Laravel'
+        $messages = [
+            [
+                'id' => 1,
+                'content' => 'Este es mi primer mensaje!',
+                'image' => 'http://placeimg.com/600/338/tech?1',
+            ],
+            [
+                'id' => 2,
+                'content' => 'Este es mi segundo mensaje!',
+                'image' => 'http://placeimg.com/600/338/tech?2',
+            ],
+            [
+                'id' => 3,
+                'content' => 'Otro mensaje más!',
+                'image' => 'http://placeimg.com/600/338/people?3',
+            ],
+            [
+                'id' => 4,
+                'content' => 'El último mensaje!',
+                'image' => 'https://picsum.photos/600/338?image=0',
+            ],
         ];
-
         return view('welcome', [
-            'teacher' => 'Guido Contreras Woda',
-            'links' => $links,
+            'messages' => $messages,
         ]);
-    }
-
-    public function about()
-    {
-        return view('about');
     }
 }
