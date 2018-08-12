245650ec5530a065aade9658df1b4169168848a7
Fernando Torres <fertorresmx@gmail.com>
2018-08-11 21:30:48 -0500

    Archivos video 18 'Relaciones belongsTo'

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -18,15 +18,13 @@ class MessagesController extends Controller
 
     public function create(CreateMessageRequest $request)
     {
-
-//        dd($request->all());
+        $user = $request->user();
 
         $message = Message::create([
+            'user_id' => $user->id,
             'content' => $request->input('message'),
             'image' => 'http://placeimg.com/600/338/tech?' . mt_rand(0,1000)
         ]);
-
-//        dd($message);
 
         return redirect('/messages/'.$message->id);
     }
