21e803cbf6f461f26b23d7277d552d62d1b314e7
Fernando Torres <fertorresmx@gmail.com>
2018-09-09 22:00:20 -0500

    Implements Algolia and search by user

--- app/Message.php
+++ app/Message.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Message extends Model
 {
+    use Searchable;
+
     protected $guarded = [];
 
     public function user()
@@ -20,5 +23,11 @@ class Message extends Model
         }
 
         return \Storage::disk('public')->url($image);
+    }
+
+    public function toSearchableArray()
+    {
+        $this->load('user');
+        return $this->toArray();
     }
 }
